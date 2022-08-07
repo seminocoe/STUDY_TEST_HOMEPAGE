4bbd1cb83d73ead1f852d0227c4fb7e6b543ce1f
seminocoe <108251010+seminocoe@users.noreply.github.com>
2022-08-07 18:03:00 +0900

    Initial commit

---
+++ BBS/src/main/java/user/UserDAO.java
@@ -0,0 +1,47 @@
+package user;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class UserDAO {
+
+	private Connection conn;
+	private PreparedStatement pstmt;
+	private ResultSet rs;
+	
+	public UserDAO() {
+		try {
+			String dbURL = "jdbc:oracle:thin:@localhost:1521:orcl";
+			String dbID = "scott";
+			String dbPassword = "tiger";
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			conn = DriverManager.getConnection(dbURL, dbID, dbPassword);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public int login(String userID, String userPassword) {
+		String SQL = "SElect userPassword From USERTEST WHERE userID = ?";
+		try {
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, userID); //보안용?잘모르겠지만 받아온 유저아이디를 넘겨준다는 듯
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				if(rs.getString(1).equals(userPassword)) {
+					return 1;//로그인 성공
+				}
+				else
+					return 0;//비밀번호 불일치
+			}
+			return -1; //아이디가 없음
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return -2; //데이터베이스 오류
+	}
+	
+}
